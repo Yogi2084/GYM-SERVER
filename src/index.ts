b74ab6cb2aafa7bc0e36cbf7e56d8fd571e19a4d
Yogananda K S <152706925+Yogi2084@users.noreply.github.com>
2026-09-15 15:46:50 +0530

    Updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,9 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import "dotenv/config";
+import { serve } from "@hono/node-server";
+import { allRoutes } from "./routes/routes-index.js";
 
-const app = new Hono()
+allRoutes.get("/", (c) => c.text("Hello from Neuronest!"));
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
-
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+serve(allRoutes, (info) => {
+  console.log(`Server is running on http://localhost:${info.port}`);
+});
